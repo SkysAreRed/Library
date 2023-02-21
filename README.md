7d6ada2a0c3744969f57115b688edcab990add43
SkysAreRed <skysarered2@gmail.com>
2023-02-21 17:27:58 +0100

    create addbook and render funcs

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Library Application made for The Odin Project
 
 ## What I have learnt so far
 Object constructors - applying these to reduce the amount of code required
-Forms - putting the knowledge of form building to practise
+Forms - putting the knowledge of form building to practise, plus adapting popupforms in JavaScript for the first time
 LocalStorage - still learning and applying
+getElementsByTagName - when and when not to apply this in my javascript
--- script.js
+++ script.js
@@ -1,28 +1,73 @@
-const books = document.querySelector("book");
+const newBookBtn = document.querySelector('#newBtn');
+newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
 
-let library;
-let DEFAULT_DATA = [
-    {
-        title: "book1",
-        author: "book1",
-        read: true,
-    },    
-    {
-        title: "book2",
-        author: "book2",
-        read: false,
-    }
-];
+const addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', addBookToLibrary);
 
-class book { // book constructor
+const popUpForm = document.getElementById('popUp');
+const closePopUp = document.getElementByTagName('span')[0];
+closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
+
+
+class Book { // book constructor
     constructor(title, author, read) {
-        this.title = title;
-        this.author = author;
-        this.read = read;
+        this.title = form.title.value;
+        this.author = form.author.value;
+        this.read = form.read.checked;
     }
 }
+
+let myLibrary = [
+    title
+];
+let newBook;
 
 function addBookToLibrary() {
-    const newBook = new Book(title.value, author.value, read.value);
-    library.push(newBook);
+    //event.preventDefault(); // makes event cancellable
+    popUpForm.style.display = 'none';
+
+    newBook = new Book(title, author, read);
+    myLibrary.push(newBook); // pushes newBook into myLibrary array 
+
+    render(); // renders page to update book display 
+    form.reset(); // clears previously entered data
 }
+
+function render() { // creates book visually in your browser
+    const display = document.getElementById('book-container');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild(book)); 
+
+    for (let i=0; i < myLibrary.length; i++) { // loops through createBook function for each book card
+        createBook(myLibrary[i])
+    }
+}
+
+function createBook(item) {
+    const library = document.querySelector('#book-container');
+    const aBookDiv = document.createElement('div');
+
+    const blockshadingDiv = document.createElement('div'); // this goes inside 
+    
+    const infoblockDiv = document.createElement('div'); // along with this which contains the array information for each book
+
+    // these happen inside infoblockDiv
+    const titleDiv = document.createElement('div')
+    const authDiv = document.createElement('div')
+    const removeBtn = document.createElement('button')
+    const readBtn = document.createElement('button')
+
+    const userBookActionsDiv = document.createElement('div'); // this holds the dummy images
+
+    aBookDiv.classList.add('aBook'); // add class name to new div
+    aBookDiv.setAttribute('id', myLibrary.indexOf(item)); // give each book a number to organise array'
+
+
+    library.appendChild(aBookDiv); // appends aBookDiv to be the child of library
+}
+
+createBook()
+render()
+
+
+//pick up from createBook and add divs to make sexy book cards
