cc28043c029d4cb80612356733262ad69939f5d9
SkysAreRed <skysarered2@gmail.com>
2023-02-23 11:06:49 +0100

    fix getelementbytagname bug

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const newBookBtn = document.querySelector('#newBtn');
 newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
 
 const popUpForm = document.getElementById('popUp');
-const closePopUp = document.getElementByTagName('span')[0];
+const closePopUp = document.getElementsByTagName('span')[0];
 closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
 
 
-// class Book { // book constructor
-//     constructor(title, author, read) {
-//         this.title = form.title.value;
-//         this.author = form.author.value;
-//         this.read = form.read.checked;
-//     }
-// }
+class Book { // book constructor
+    constructor(title, author, read) {
+        this.title = form.title.value;
+        this.author = form.author.value;
+        this.read = form.read.checked;
+    }
+}
 
 let myLibrary = [
     {title: "potato", author: "potato2", read: false}];
@@ -53,8 +53,8 @@ function createBook(item) {
     // these happen inside infoblockDiv
     const titleDiv = document.createElement('div')
     const authDiv = document.createElement('div')
-    //const removeBtn = document.createElement('button')
-    //const readBtn = document.createElement('button')
+    const removeBtn = document.createElement('button')
+    const readBtn = document.createElement('button')
 
     const userBookActionsDiv = document.createElement('div'); // this holds the dummy images
 
@@ -69,6 +69,16 @@ function createBook(item) {
 
     infoblockDiv.appendChild(titleDiv);
     infoblockDiv.appendChild(authDiv);
+
+    removeBtn.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(item), 1);
+        render();
+    })
+
+    readBtn.addEventListener('click', () => {
+        item.read = !item.read;
+        render();
+    })
 }
 
 render()
